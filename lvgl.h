2683276038550ce6d3afecd90244757446139d4c
i12309 <i12309@bk.ru>
2026-04-21 11:28:46 +0300

    -

---
+++ lvgl.h
@@ -0,0 +1,7 @@
+#ifndef SCREEN32_LVGL_COMPAT_ROOT_LVGL_H
+#define SCREEN32_LVGL_COMPAT_ROOT_LVGL_H
+
+/* Compatibility shim for includes that expect <lvgl.h> at project include roots. */
+#include "lib/ScreenUI/vendor/lvgl-release-v9.4/lvgl-release-v9.4/lvgl.h"
+
+#endif /* SCREEN32_LVGL_COMPAT_ROOT_LVGL_H */
---
+++ lvgl/lvgl.h
@@ -0,0 +1,7 @@
+#ifndef SCREEN32_LVGL_COMPAT_LVGL_H
+#define SCREEN32_LVGL_COMPAT_LVGL_H
+
+/* Compatibility shim for code generators that include <lvgl/lvgl.h>. */
+#include "../lib/ScreenUI/vendor/lvgl-release-v9.4/lvgl-release-v9.4/lvgl.h"
+
+#endif /* SCREEN32_LVGL_COMPAT_LVGL_H */
